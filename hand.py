b532d105865e0b0632378eefa78b8dad66bc898b
_altos <markjomer016@gmail.com>
2025-12-24 19:18:06 +0800

    Update hand.py

--- hand.py
+++ hand.py
@@ -7,10 +7,9 @@ class Hand:
 
     def total(self):
         totalval = sum(card._value for card in self._cardinhand)
-        # forgot how to simplify codes in one liners
         acecount = sum(1 for card in self._cardinhand if card._rank == 'A')
         while totalval > 21 and acecount > 0:
-            totalval -= 10  # handles the 1 value when its over
+            totalval -= 10
             acecount -= 1
         return totalval
 
@@ -18,7 +17,6 @@ class Hand:
 # checkers
 
     def is_bj(self):
-        # forgot the rules of whats a blackjack and payout sh
         return self.total() == 21 and len(self._cardinhand) == 2
 
     def is_bust(self):
